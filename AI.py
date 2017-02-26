5aead4a9f478aa5672c6848b562e328cf2efb0e3
trentsmith <trent.s.2012@knights.ucf.edu>
2017-02-26 15:54:14 -0500

    Delete AI.py

--- AI.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import datetime
-conn=sqlite3.connect('robot.db')
-c=conn.cursor()
-def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS hello(input TEXT, output TEXT,emotion TEXT)')
-    c.execute('CREATE TABLE IF NOT EXISTS smalltalk(input TEXT, output TEXT,emotion TEXT)')
-    c.execute('CREATE TABLE IF NOT EXISTS conclusion(input TEXT, output TEXT,emotion TEXT)')
-create_table()
