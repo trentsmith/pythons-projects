e9f7e4ee5729c4c0e271c0656452645478906ac4
trentsmith <smithtrent00@gmail.com>
2017-07-05 09:59:57 -0700

    Update camshaft.py

--- camshaft.py
+++ camshaft.py
@@ -1,38 +1,38 @@
-#note this project is still in progress
 import numpy as np
 import cv2
-cap = cv2.VideoCapture('hello.flv')
-# take first frame of the video
-ret,frame = cap.read()
-# setup initial location of window
-r,h,c,w = 250,90,400,125  # simply hardcoded the values
+import matplotlib.pyplot as plt
+import Tkinter as tk
+cap = cv2.VideoCapture(0)
+r=250
+h=90
+c=400
+w=125  # simply hardcoded the values
 track_window = (c,r,w,h)
-# set up the ROI for tracking
-roi = frame[r:r+h, c:c+w]
-hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
-# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-while(1):
-    ret ,frame = cap.read()
-    if ret == True:
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-        # apply meanshift to get the new location
-        ret, track_window = cv2.CamShift(dst, track_window, term_crit)
-        # Draw it on image
-        pts = cv2.boxPoints(ret)
-        pts = np.int0(pts)
-        img2 = cv2.polylines(frame,[pts],True, 255,2)
-        cv2.imshow('img2',img2)
-        k = cv2.waitKey(60) & 0xff
-        if k == 27:
-            break
-        else:
-            cv2.imwrite(chr(k)+".jpg",img2)
-    else:
+x = 50
+y = 40
+while(True):
+    ret, frame = cap.read()
+    roi = frame[0:100,0:100]
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, np.array((50., 50.,50.)), np.array((255.,255.,255.)))
+    cv2.circle(mask,(x,y), 63,(0,0,255), -1)
+    cv2.imshow('frame',mask)
+    iar = np.asarray(mask)
+    i =0
+   #'''while i<len(mask):
+    #    j=0
+     #   while j<len(mask[i]):
+      #      j=j+1
+      #  i=i+1'''
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
         break
-cv2.destroyAllWindows()
+    if (cv2.waitKey(1) & 0xFF == ord('a')):
+        x = x-10
+    if (cv2.waitKey(1) & 0xFF == ord('s')):
+        y = y-10
+    if (cv2.waitKey(1) & 0xFF == ord('d')):
+        x = x+10
+    if (cv2.waitKey(1) & 0xFF == ord('w')):
+        y = y+10
 cap.release()
+cv2.destroyAllWindows()
